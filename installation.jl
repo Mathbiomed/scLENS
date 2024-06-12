7666455fd835d02d18a4f1e9f767a33817555716
hkimman <105997427+hkimman@users.noreply.github.com>
2024-06-12 14:11:01 +0900

    Update installation.jl
    Specify the version of CUDA.jl due to the error.

--- installation.jl
+++ installation.jl
@@ -1,5 +1,5 @@
 using Pkg
- Pkg.add(["ArgParse","DataFrames","Pandas","CUDA@5.2.0","SparseArrays","Glob","JLD2","CodecLz4","StatsBase", "Muon","RCall","Makie","CairoMakie","ColorSchemes", 
- "Random", "ProgressMeter", "LinearAlgebra", "UMAP", "Distances", "InlineStrings", "Clustering", "CSV", "NPZ", "Colors", "Printf","Suppressor", "NaNStatistics",
- "MatrixMarket", "GZip","Distributions"])
+Pkg.add(Pkg.PackageSpec(;name="CUDA", version="5.2.0"))
+Pkg.add(["ArgParse","DataFrames","Pandas","SparseArrays","Glob","JLD2","CodecLz4","StatsBase", "Muon","RCall","Makie","CairoMakie","ColorSchemes",
+  "Random", "ProgressMeter", "LinearAlgebra", "UMAP", "Distances", "InlineStrings", "Clustering", "CSV", "NPZ", "Colors", "Printf","Suppressor", "NaNStatistics","MatrixMarket", "GZip","Distributions"])
 
